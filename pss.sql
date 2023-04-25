ee77a6ac65ee432223bc9a47e8949b71e7ecffe8
Jeof Petty <jeofpetty@icloud.com>
2023-04-24 20:21:26 -0400

    updated pss.sql to add seed file, created fake starter data for dev

--- pss.sql
+++ pss.sql
@@ -6,7 +6,7 @@ CREATE DATABASE press_start_society;
 \connect press_start_society
 
 \i pss-schema.sql
--- \i pss-seed.sql
+\i pss-seed.sql
 
 \echo 'Delete and recreate press_start_society_test db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
