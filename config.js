66b197b7cfa59e139e3112a78ed4c343a7444832
Jeof Petty <jeofpetty@icloud.com>
2023-04-27 18:19:38 -0400

    updated deps, add api_base to config

--- config.js
+++ config.js
@@ -4,6 +4,8 @@
 
 require("dotenv").config();
 
+const API_BASE_URL = "https://api.rawg.io/api"
+
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
 const PORT = +process.env.PORT || 3001;
@@ -16,6 +18,8 @@ function getDatabaseUri() {
 }
 
 module.exports = {
+    API_BASE_URL,
+    getDatabaseUri,
     SECRET_KEY,
     PORT
 }
