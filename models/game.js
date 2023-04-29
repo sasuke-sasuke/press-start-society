4ac41dcc555f879067bc7a7991d8594e9a853407
Jeof Petty <jeofpetty@icloud.com>
2023-04-28 20:36:41 -0400

    added better search functionality

--- models/game.js
+++ models/game.js
@@ -2,53 +2,56 @@
 const axios = require("axios")
 const {API_BASE_URL} = require("../config")
 const {RAWG_KEY} = require("../api_key")
-const {BadRequestError} = require('../expressError')
-const db = require("../db")
+const {BadRequestError, NotFoundError} = require('../expressError')
 
 /** Related functions to Game */
 
 class Game {
 
-    // get all games
-    static async getAllGames() {
-        try {
-            const res = await axios.get(`${API_BASE_URL}/games?key=${RAWG_KEY}`);
-            return res.data;
-        } catch (err) {
-            throw new BadRequestError();
+    // To get a list of all games, enter no search filters
+    static async searchGames(searchFilters= {}) {
+        const {search, page, pageSize, platforms, genres, orderBy} = searchFilters;
+
+        const params = {
+            key: RAWG_KEY,    
         }
+
+        // If search filters are provided, add them to the params
+        search ? params.search = search : null;
+        page ? params.page = page : null;
+        pageSize ? params.page_size = pageSize : null;
+        platforms ? params.platforms = platforms.join(',') : null;
+        genres ? params.genres = genres.join(',') : null;
+        orderBy ? params.order_by = orderBy : null;
+
+        const res = await axios.get(`${API_BASE_URL}/games`, {params})
+ 
+        return res.data;
     }
 
-    // search games
-    static async searchGames(search, page=1, pageSize=10) {
-        try {
-            const res = await axios.get(`${API_BASE_URL}/games?search=${search}&page=${page}&page_size=${pageSize}&key=${RAWG_KEY}`);
-            return res.data;
-        } catch (err) {
-            throw new BadRequestError();
-        }
-    }
-
-    // gets a single game
     static async getGame(id) {
-        try {
             const res = await axios.get(`${API_BASE_URL}/games/${id}?key=${RAWG_KEY}`);
             return res.data;
-        } catch (err) {
-            throw new BadRequestError();
-        }
     }
 }
 
 // DELETE vvv
 
-// async function test(){
-//     const res = await Game.getGame(13554);
-//     console.log(res);
-//     return res.data;
-// }
-
-// let search = test();
+// const test = async() => {
+//     const searchFilters = {
+//         search: "scum",
+//         page: 1,
+//         pageSize: 10,
+//         platforms: [4, 187]  
+//     }
+//     const res = await Game.searchGames(searchFilters)
+//     if(res){
+//         console.log(res)
+//     } else {
+//         console.log("Not game found")
+//     }
+// };
+// test();
 // debugger;
 
 // DELETTE ^^
