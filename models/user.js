f4727eab764f80937c03c89fc5b8fca14049effe
Jeof Petty <jeofpetty@icloud.com>
2023-04-30 00:58:11 -0400

    included constructor and refactored methods to be instance where preferred

--- models/user.js
+++ models/user.js
@@ -7,6 +7,58 @@ const {sqlForPartialUpdate} = require("../helpers/sql")
 const {NotFoundError, BadRequestError, UnauthorizedError} = require("../expressError")
 
 class User {
+    constructor({
+        id = null,
+        username,
+        hashed_password,
+        first_name,
+        last_name,
+        email,
+        avatar = null,
+        banner_image = null,
+        about = '',
+        status = '',
+        created_at = null,
+        last_login = null,
+        follows = [],
+        followers = [],
+        posts = [],
+        favoriteGames = []
+    }) 
+    {
+        this.id = id;
+        this.username = username;
+        this.hashed_password = hashed_password;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.avatar = avatar;
+        this.banner_image = banner_image;
+        this.about = about;
+        this.status = status;
+        this.created_at = created_at;
+        this.last_login = last_login;
+        this.follows = follows;
+        this.followers = followers;
+        this.posts = posts;
+        this.favoriteGames = favoriteGames;
+    }
+
+    static async get(username) {
+        const res = await db.query(
+            `SELECT *
+            FROM users
+            WHERE username = $1`,
+            [username],
+        )
+
+        const userRow = res.rows[0];
+
+        if(!userRow) throw new NotFoundError(`User ${username} not found`);
+
+        return new User(userRow);
+    }
+
 
     static async register(username, password, firstName, lastName, email){
         const duplicateCheck = await db.query(
@@ -17,7 +69,7 @@ class User {
         );
 
         if(duplicateCheck.rows[0]) {
-            throw new BadRequest(`Username ${username} is already taken`);
+            throw new BadRequestError(`Username ${username} is already taken`);
         }
 
         const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
@@ -27,55 +79,55 @@ class User {
             (username, hashed_password, first_name, last_name, email)
             VALUES
             ($1, $2, $3, $4, $5)
-            RETURNING 
-            username,
-            first_name AS "firstName", 
-            last_name AS "lastName", 
-            email`,
+            RETURNING *`,
             [username, hashedPassword, firstName, lastName, email]
         );
-        const user = res.rows[0]
 
-        return user;
+        const newUserRow = res.rows[0];
+        const newUser = new User(newUserRow);
+
+        // Update the instance properties with updated data
+        for (const [key, value] of Object.entries(newUserRow)){
+            newUser[key] = value;
+        }
+
+        return newUser;
     }
 
 
     static async login(username, password){
         const res = await db.query(
-            `SELECT username, hashed_password 
+            `SELECT * 
             FROM users
             WHERE username = $1`,
             [username],
         );
-        const user = res.rows[0];
+        const userRow = res.rows[0];
 
-        if(user) {
-            const valid = await bcrypt.compare(password, user.hashed_password);
-            if(valid){
-                delete user.hashed_password;
-                return user;
-            }
-        }
+        if(!userRow) throw new UnauthorizedError(`Invalid username or password`);
 
-        throw new UnauthorizedError("Invalid username or password");
-    }
+        const isValid = await bcrypt.compare(password, userRow.hashed_password);
 
-    static async get(username) {
-        const res = await db.query(
-            `SELECT username
-            FROM users
-            WHERE username = $1`,
-            [username],
-        )
+        if(!isValid) throw new UnauthorizedError(`Invalid username or password`);
 
-        const user = res.rows[0];
+        const user = new User(userRow);
 
-        if(!user) throw new NotFoundError(`User ${username} not found`);
+        const lastLoginRes = await db.query(
+            `UPDATE users
+            SET last_login = NOW()
+            WHERE id = $1
+            RETURNING
+            last_login`,
+            [user.id]
+        );
+
+        user.last_login = lastLoginRes.rows[0].last_login;
 
         return user;
     }
 
-    static async update(username, data) {
+
+    async update(data) {
         if (data.password) {
             data.hashed_password = await bcrypt.hash(data.password, BCRYPT_WORK_FACTOR);
         }
@@ -83,43 +135,108 @@ class User {
         const { setCols, values } = sqlForPartialUpdate(
             data,
             {
+                username: "username",
+                hashed_password: "hashed_password",
                 firstName: "first_name",
                 lastName: "last_name",
                 email: "email",
                 avatar: "avatar",
                 bannerImage: "banner_image",
-                about: "about"
+                about: "about",
+                status: "status"
             });
         const usernameVarIdx = "$" + (values.length + 1);
 
         const querySql = `UPDATE users
                           SET ${setCols}
                           WHERE username = ${usernameVarIdx}
-                          RETURNING username,
-                          first_name AS "firstName", 
-                          last_name AS "lastName", 
-                          email`;
+                          RETURNING *`;
 
-        const res = await db.query(querySql, [...values, username]);
-        const user = res.rows[0];
+        const res = await db.query(querySql, [...values, this.username]);
+        const updatedUserRow = res.rows[0];
 
-        if(!user) throw new NotFoundError(`User ${username} not found`);
+        if(!updatedUserRow) throw new NotFoundError(`User ${username} not found`);
 
-        delete user.password;
-        return user;
+        // Update the instance properties with updated data
+        for (const [key, value] of Object.entries(updatedUserRow)) {
+            this[key] = value;
+        }
+
+        return this;
     }
 
-    static async remove(username) {
+    async permaRemove() {
         const res = await db.query(
             `DELETE FROM users
             WHERE username = $1
             RETURNING username`,
-            [username],
+            [this.username],
         )
 
-        const user = res.rows[0];
+        const removedUserRow = res.rows[0];
 
-        if(!user) throw new NotFoundError(`User ${username} not found`);
+        if(!removedUserRow) throw new NotFoundError(`User ${username} not found`);
+
+        return {
+            message:`${removedUserRow.username} has been removed`
+        }
+    }
+
+    async getFavGames(){
+        const res = await db.query(
+            `SELECT g.title  
+             FROM users u 
+             JOIN favorite_games f 
+             ON u.id = f.user_id 
+             JOIN games g 
+             ON f.game_id = g.id
+             WHERE username = $1`,
+             [this.username],
+        )
+        const userFavGamesRows = res.rows;
+        this.favoriteGames = userFavGamesRows;
+        return this;
+    }
+
+
+    async addFavGame(gameId){
+
+        await db.query(
+            `INSERT INTO favorite_games
+            (user_id, game_id)
+            VALUES
+            ($1, $2)`,
+            [this.id, gameId]
+        )
+
+        return this.getFavGames();
+    }
+
+
+    async removeFavGame(gameId){
+
+        await db.query(
+            `DELETE FROM favorite_games
+            WHERE user_id = $1 AND game_id = $2`,
+            [this.id, gameId]
+        )
+
+        return this.getFavGames();
+    }
+
+    static async getFollows(username) {
+    }
+
+    static async getFollowers(username){
+
+    }
+
+    async follow(usernameToFollow){
+
+    }
+    
+    async unfollow(usernameToUnfollow){
+
     }
 
 }
@@ -127,20 +244,29 @@ class User {
 // DELETE vvvvvvvv
 
 // const test = async () => {
-//     const res = await User.register(
-//         "sasuke",
-//         "myeye123",
-//         "Sasuke",
-//         "Uchiha",
-//         "sasuke@email.com"
-//     )
-//     const res = await User.remove("johndoe");
-//     const res = await User.login("sasuke", "myeye123");
-//     console.log(res);
+    // const res = await User.register(
+    //     "bob",
+    //     "1234567",
+    //     "bob",
+    //     "bobby boy",
+    //     "bob@email.com"
+    // )
+        // const res = await User.get("seven");
+    //     const user = await User.login("bob", "1234567");  
+    // console.log(await user.permaRemove("bob"));
+    // const user = await User.login("three", "1234567");
+    // console.log(await user.update({
+    //     firstName: "chrys",
+    //     about: "super cute"
+    // }))
+    // console.log(await user.getFavGames())
+    // console.log(await user.addFavGame(9));
+    // console.log(await user.removeFavGame(7));
+    // console.log(res);
+    // console.log(user)
 // }
 
 // test();
-// debugger;
 
 // DELETE ^^^^^^^^
 
